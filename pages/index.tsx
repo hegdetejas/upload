ca2c619b1e34ee1b6b7a6b40958440229e26e826
Tejas Hegde <tejas@Tejas-MacBook.local>
2021-12-27 15:55:08 -0600

    Added title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,12 @@ import type { NextPage } from 'next';
 
 export default function Home() {
   return (
-    <div className="text-2xl text-red-500">
-      Hello World
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-center pt-7 pb-7 bg-background">
+        <div>
+          <h2 className="text-4xl text-green-300 font-light animate-pulse">PhotoBank</h2>
+        </div>
+      </div>
     </div>
   )
 }
